d125d109beb275a2b3cd331f558450733cc1d366
ruanjiayu <1963241849@qq.com>
2019-08-29 11:05:29 +0800

    第一次提交

---
+++ src/main/java/com/xian/demo/hello.java
@@ -0,0 +1,14 @@
+package com.xian.demo;
+
+/**
+ * @Description:
+ * @Author: Xian
+ * @CreateDate: 2019/8/29  11:00
+ * @Version: 0.0.1-SHAPSHOT
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
